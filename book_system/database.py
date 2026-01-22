f192b926994edae91c6c72d3d685a3d26f45f7c5
Autumn <1667877328@qq.com>
2026-01-22 10:42:24 +0800

    添加了web（fastapi）容器重启机制，等待mysql启动

--- book_system/database.py
+++ book_system/database.py
@@ -1,12 +1,24 @@
 from sqlmodel import create_engine, Session
-import os
+from sqlalchemy.exc import OperationalError
+import os, time
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "mysql+pymysql://root:root@127.0.0.1:3306/book_system"
     )
-engine = create_engine(DATABASE_URL, echo=True)
 
+def get_engine_with_retry():
+    while True:
+        try:
+            engine = create_engine(DATABASE_URL, echo=True)
+            with engine.connect() as conn:
+                print("✅ 数据库连接成功！")
+                return engine
+        except OperationalError:
+            print("⏳ 数据库正在启动中... 等待 3 秒后重试...")
+            time.sleep(3)
+
+engine = get_engine_with_retry()
 
 def get_session():
     with Session(engine) as session:
